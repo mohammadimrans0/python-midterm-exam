8a173f326369289f01c4d127fd4eb18a7bc94b4f
Mohoammad Imran <mohoammadimran@Mohoammads-MacBook-Air.local>
2024-10-28 19:21:08 +0600

    first and second task completed

--- star_cinema.py
+++ star_cinema.py
@@ -3,3 +3,15 @@ class Star_Cinema:
 
     def entry_hall(self, hall):
         self.hall_list.append(hall)
+
+class Hall(Star_Cinema):
+    def __init__(self, rows, cols, hall_no):
+        super().__init__()
+        self.rows = rows                 
+        self.cols = cols                  
+        self.hall_no = hall_no         
+        self.seats = {}                   
+        self.show_list = []              
+
+        # insert this hall instance into Star_Cinema's hall_list
+        self.entry_hall(self)
